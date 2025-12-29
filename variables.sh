18c57ec799734333bf6b2d8da7a485a5a01317c7
Venkataraja0369 <venkatarajat0369@gmail.com>
2025-12-29 15:50:31 +0530

    code

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Please enter username"
+read -s USERNAME
+echo "entered username: $USERNAME"
+
+echo  "Please enter your password"
+read -s PASSWORD
+echo "entered password: $PASSWORD"
